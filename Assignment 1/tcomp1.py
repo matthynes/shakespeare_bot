374bfaaf43e3a02ad1d8b74a53600a203bcb2155
Matt Hynes <matthynesdev@gmail.com>
2016-09-14 20:15:58 -0230

    Add working(?) similarity calculation

--- Assignment 1/tcomp1.py
+++ Assignment 1/tcomp1.py
@@ -2,6 +2,8 @@ import os
 import sys
 
 # get relevant command line arguments
+import collections
+
 args = sys.argv[1:]
 
 # check if enough args given
@@ -16,21 +18,46 @@ def ngramify(file, n):
     with open(__location__) as f:
         lines = f.read().splitlines()
         for line in lines:
-            line = list(line)
+            # line = list(line)
             ngram += list(zip(*[line[i:] for i in range(n)]))
     return ngram
 
 
 def sim(ngram1, ngram2):
-    pass
+    # freq1 = collections.Counter(ngram1)
+    # freq2 = collections.Counter(ngram2)
+    #
+    # ng1s = float(len(ngram1))
+    # ng2s = float(len(ngram2))
+    #
+    # for f, n in freq1.items():
+    #     freq1[f] = (n / ng1s)
+    # for f, n in freq2.items():
+    #     freq2[f] = (n / ng2s)
+    # c = 0
+    # for ff, nn in freq1.items():
+    #     if ff in freq2:
+    #         c += (nn + freq2[ff])
+    #
+    # return 1.0 - (c)
+    ngset1 = set(ngram1)
+    ngset2 = set(ngram2)
+
+    common = ngset1.intersection(ngset2)
+    all = ngset1.union(ngset2)
+
+    if common:
+        return round(len(common) / float(len(all)), 3)
+    else:
+        return 0.0
 
 
 if __name__ == '__main__':
     master_file = args[0]
-    n = args[1]
+    n = int(args[1])
     file_list = args[2:]
 
     ngram_master = ngramify(master_file, n)
     for file in file_list:
         ngram_file = ngramify(file, n)
-        print sim(ngram_master, ngram_file)
+        print 'Sim("', master_file, '","', file, '")=', sim(ngram_master, ngram_file)
