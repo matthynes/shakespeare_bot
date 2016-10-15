4a5d5742b51b5cea708121ff9f7715e4980847a1
Matt Hynes <matthynesdev@gmail.com>
2016-10-15 15:53:07 -0230

    First draft of composition function

--- Assignment 2/reconstruct.py
+++ Assignment 2/reconstruct.py
@@ -32,8 +32,32 @@ def readFST(filename):
 
 
 def composeFST(fst1, fst2):
-    for q in fst1:
-        pass
+    fr = OrderedDict()
+    start = OrderedDict()
+    # start[fst1.keys()[0] + ',' + fst2.keys()[0]] = OrderedDict('')
+
+    f1_keys = fst1.keys()
+    f2_keys = fst2.keys()
+
+    for n in range(len(fst1)):
+
+        q1 = fst1[f1_keys[n]]
+        q2 = fst2[f2_keys[n]]
+        comp = f1_keys[n] + ',' + f2_keys[n]
+
+        for m in range(len(q1)):
+            t1 = q1.items()[m]
+            t2 = q2.items()[m]
+
+            trans1 = t1[0]
+            trans2 = t2[0]
+
+            if trans1.split('/')[1] == trans2.split('/')[0]:
+                fr[comp] = OrderedDict()
+                fr[comp][(trans1.split('/')[1] + '/' + trans2.split('/')[0])] = (
+                    t1[1] + ',' + t2[1])
+
+    return fr
 
 
 def reconstructUpper(l, fst):
@@ -41,15 +65,28 @@ def reconstructUpper(l, fst):
         trans = fst[q].keys()
         chars = [t.split('/') for t in trans]
         upper = [c[0] for c in chars]
+        lower = [c[1] for c in chars]
 
-        print upper
+        for i in range(len(lower)):
+            if l == lower[i]:
+                print upper[i]
 
 
-def reconstructLower(l, fst):
-    pass
+def reconstructLower(u, fst):
+    for q in fst:
+        trans = fst[q].keys()
+        chars = [t.split('/') for t in trans]
+        upper = [c[0] for c in chars]
+        lower = [c[1] for c in chars]
+
+        for i in range(len(upper)):
+            if u == upper[i]:
+                print lower[i]
 
 
 if __name__ == '__main__':
-    f = readFST(sys.argv[1])
+    f = readFST('test1.fst')
+    f2 = readFST('test2.fst')
 
-    reconstructUpper('a', f)
+    fr = composeFST(f, f2)
+    pass
