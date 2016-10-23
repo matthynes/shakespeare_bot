3552fe93863c41c898140e5b8c0165ae073d6084
Matt Hynes <matthynesdev@gmail.com>
2016-10-23 17:19:03 -0230

    Compose multiple FSTs

--- Assignment 2/reconstruct.py
+++ Assignment 2/reconstruct.py
@@ -3,11 +3,14 @@ import os
 import sys
 from collections import OrderedDict
 
+import itertools
+
 args = sys.argv[1:]
 
 # check if enough args given
 if len(args) < 3:
-    print 'usage: python tcomp1.py filename n filename1 filename2 ...'
+    print 'usage: python reconstruct.py surface/lexical wlf F1 ... Fn'
+    sys.exit(1)
 
 
 def readFST(filename):
@@ -39,13 +42,11 @@ def readFST(filename):
 
 def composeFST(fst1, fst2):
     fr = OrderedDict()
-    start = OrderedDict()
-    # start[fst1.keys()[0] + ',' + fst2.keys()[0]] = OrderedDict('')
 
     f1_keys = fst1.keys()
     f2_keys = fst2.keys()
 
-    for n in range(len(fst1)):
+    for n in range(min(len(fst1), len(fst2))):
 
         q1 = fst1[f1_keys[n]]
         q2 = fst2[f2_keys[n]]
@@ -59,9 +60,11 @@ def composeFST(fst1, fst2):
             trans2 = t2[0]
 
             if trans1.split('/')[1] == trans2.split('/')[0]:
+
                 if comp not in fr:
                     fr[comp] = OrderedDict()
-                fr[comp][(trans1.split('/')[1] + '/' + trans2.split('/')[0])] = t1[1] + ',' + t2[1]
+                fr[comp][(trans1.split('/')[1] + '/' + trans2.split('/')[0])] = t1[1] + ',' \
+                                                                                        '' + t2[1]
 
     return fr
 
@@ -77,7 +80,7 @@ def reconstructUpper(l, fst):
             for i in range(len(lower)):
                 if ch == lower[i] or (lower[i] == '-' or ch == '-'):
                     if q[2] == 'F':
-                        print upper[i]
+                        sys.stdout.write(upper[i])
 
 
 def reconstructLower(u, fst):
@@ -91,11 +94,16 @@ def reconstructLower(u, fst):
             for i in range(len(upper)):
                 if ch == upper[i] or (upper[i] == '-' or ch == '-'):
                     if q[2] == 'F':
-                        print lower[i]
+                        sys.stdout.write(lower[i])
 
 
 if __name__ == '__main__':
     sur_low = args[0]
+
+    if sur_low != 'surface' and sur_low != 'lexical':
+        print 'Must specify if reconstruction is surface or lexical.'
+        sys.exit(1)
+
     wlsf = args[1]
     file_list = args[2:]
 
@@ -104,6 +112,30 @@ if __name__ == '__main__':
     for f in file_list:
         __path__ = os.path.realpath(
             os.path.join(os.getcwd(), os.path.dirname(__file__), f))
-        fsts.append(readFST(f))
+        fsts.append(readFST(__path__))
 
-    pass
+    fst = fsts[0]
+
+    for i in range(1, len(fsts)):
+        fst = composeFST(fst, fsts[i])
+
+    num_trans = 0
+    for s in fst:
+        num_trans += len(fst[s].keys())
+    print "Composed FST has " + str(len(fst.keys())) + " states and " + str(num_trans) + \
+          " transitions.\n"
+
+    __path__ = os.path.realpath(
+        os.path.join(os.getcwd(), os.path.dirname(__file__), wlsf))
+
+    with open(__path__) as wf:
+        for line in wf.readlines():
+            line = line.strip()
+            print "Reconstructing " + sur_low + " form for " + line + ":"
+            sys.stdout.write('\t')
+            for ch in line:
+                if sur_low == 'surface':
+                    reconstructUpper(ch, fst)
+                else:
+                    reconstructLower(ch, fst)
+            print '\n'
