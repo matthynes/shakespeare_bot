daed036276b7ab74d80ce2f18c8c2274486da861
Matt Hynes <matthynesdev@gmail.com>
2016-11-05 10:33:39 -0230

    Implement dictionary-based implementation for parsing the grammar itself

--- Assignment 3/CKYdet.py
+++ Assignment 3/CKYdet.py
@@ -8,6 +8,19 @@ args = sys.argv[1:]
 if len(args) != 2:
     print 'usage: python CKYdet.py eCNF_file utterance_file'
 
+
+def CKYparse(ckyf, uttf):
+    __path__ = os.path.realpath(
+        os.path.join(os.getcwd(), os.path.dirname(__file__), ckyf))
+
+    grammar = collections.defaultdict(str)
+
+    with open(__path__, 'r') as cf:
+        for line in cf.readlines():
+            rhs, lhs = line.strip().split(' -> ')
+            grammar[rhs] = lhs
+    
+
 if __name__ == '__main__':
     ckyfile = args[0]
     uttfile = args[1]
@@ -21,3 +34,5 @@ if __name__ == '__main__':
     if not ext == '.utt':
         print 'Utterance file must be of .utt type.'
         sys.exit(1)
+
+    CKYparse(ckyfile, uttfile)
