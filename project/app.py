bc14a1759a578d986dc73405da15d8039b5a338e
Matt Hynes <matthynesdev@gmail.com>
2016-11-20 13:03:30 -0330

    Generate test corpus and set up Markovify text model

--- project/app.py
+++ project/app.py
@@ -1,10 +1,17 @@
+import os
+
 import tweepy
+import markovify
+
 from settings import CONSUMER_SECRET, CONSUMER_KEY, ACCESS_TOKEN_SECRET, ACCESS_TOKEN
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
+
+with open(os.path.dirname(__file__) + '/corpus.txt', 'r') as corpus:
+    model = markovify.Text(corpus.read())
 
 public_tweets = api.home_timeline()
 for tweet in public_tweets:
