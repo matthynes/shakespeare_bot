c1e01d2886a9aa500e5053ce974788ef05995fd0
Matt Hynes <matthynesdev@gmail.com>
2016-11-21 14:50:43 -0330

    Generate post and post to Twitter on interval

--- project/app.py
+++ project/app.py
@@ -1,5 +1,5 @@
 import os
-
+import time
 import tweepy
 import markovify
 
@@ -10,9 +10,23 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
 
-with open(os.path.dirname(__file__) + '/corpus.txt', 'r') as corpus:
-    model = markovify.Text(corpus.read())
+STATE_SIZE = 2
+TWEET_INTERVAL = 60 * 60  # 60 * n = n-minute intervals
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+with open(os.path.dirname(__file__) + '/corpus.txt', 'r') as corpus:
+    model = markovify.Text(corpus.read(), state_size=STATE_SIZE)
+
+public_tweets = api.user_timeline(count=24)
+
+
+def make_post():
+    post = model.make_short_sentence(140)
+
+    if post not in public_tweets:
+        api.update_status(post)
+
+
+if __name__ == '__main__':
+    while True:
+        make_post()
+        time.sleep(TWEET_INTERVAL)
